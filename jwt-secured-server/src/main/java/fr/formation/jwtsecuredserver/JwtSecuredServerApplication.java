214887dc87448bf9fada821209b967e3113a7f3a
Frank MARSHALL <frank@readresolve.io>
2020-07-31 18:34:40 +0200

    Fixed cyclic dependency (password encoder)

--- jwt-secured-server/src/main/java/fr/formation/jwtsecuredserver/JwtSecuredServerApplication.java
+++ jwt-secured-server/src/main/java/fr/formation/jwtsecuredserver/JwtSecuredServerApplication.java
@@ -2,6 +2,9 @@ package fr.formation.jwtsecuredserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootApplication
 // Disable security:
@@ -10,5 +13,15 @@ public class JwtSecuredServerApplication {
 
     public static void main(String[] args) {
 	SpringApplication.run(JwtSecuredServerApplication.class, args);
+    }
+
+    /**
+     * The password encoder bean for the application.
+     *
+     * @return a password encoder
+     */
+    @Bean
+    protected PasswordEncoder passwordEncoder() {
+	return new BCryptPasswordEncoder();
     }
 }
